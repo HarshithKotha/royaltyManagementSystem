6f9cf7ecdc6201bf01d2e8b461903b549dbb71b2
HarshithKotha <95026901+HarshithKotha@users.noreply.github.com>
2025-04-22 11:24:02 +0530

    Update AuthServices.jsx

--- RMS_finalCut/frontend/src/services/AuthServices.jsx
+++ RMS_finalCut/frontend/src/services/AuthServices.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/api/auth";
+const API_URL = "https://authenticationmodule-4043.onrender.com/api/auth";
 
 export const loginUser = async (email, password) => {
     try {
